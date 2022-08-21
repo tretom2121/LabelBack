4ca7d760a3fbc0cb0cde5a15e6c119076540ca4c
tt <t.trencsenyi@gmail.com>
2022-08-21 22:10:16 +0200

    Add project files.

---
+++ LabelBack/ContentToColorConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace LabelBack
+{
+    public class ContentToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value.ToString() == "Failed") return Brushes.LightCoral;
+            return Brushes.LightGreen;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
