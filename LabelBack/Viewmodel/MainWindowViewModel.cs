55d80b11aa39824504afbb385adc0c42ab3c0774
tt <t.trencsenyi@gmail.com>
2022-09-12 10:30:46 +0200

    tab item visibility

--- LabelBack/Viewmodel/MainWindowViewModel.cs
+++ LabelBack/Viewmodel/MainWindowViewModel.cs
@@ -20,6 +20,8 @@ namespace LabelBack.Viewmodel
 
                 LabelContent = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
             });
+
+            SwitchTabItem1VisibilityCommand = new DelegateCommand(exec => TabItem1Visibility = !TabItem1Visibility);
         }
 
         private string _labelContent;
@@ -32,8 +34,21 @@ namespace LabelBack.Viewmodel
                 OnPropertyChanged();
             }
         }
-
-
         public DelegateCommand RollContentCommand { get; set; }
+        
+        
+        public DelegateCommand SwitchTabItem1VisibilityCommand { get; set; }
+
+        private bool _tabItem1Visibility;
+
+        public bool TabItem1Visibility
+        {
+            get { return _tabItem1Visibility; }
+            set 
+            { 
+                _tabItem1Visibility = value; 
+                OnPropertyChanged(); 
+            }
+        }
     }
 }
